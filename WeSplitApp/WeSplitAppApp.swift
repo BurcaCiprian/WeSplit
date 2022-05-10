889b24650f3209c9a1ff184aac23af9814413a47
Victor Ciprian Burca <victor.ciprian.burca1@ibm.com>
2022-05-10 16:56:01 +0300

    Initial Commit

---
+++ WeSplitApp/WeSplitAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitAppApp.swift
+//  WeSplitApp
+//
+//  Created by Victor Ciprian Burca on 10.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
